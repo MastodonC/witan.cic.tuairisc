9d58caeab91094a0798c268493caffc70aa4d9ca
Bruce Durling <bruce@mastodonc.com>
2022-04-25 17:17:01 +0100

    Refactored to allow for placement analysis

--- src/witan/cic/tuairisc/care_days.clj
+++ src/witan/cic/tuairisc/care_days.clj
@@ -1,9 +1,10 @@
-(ns witan.cic.tuairisc.age-and-placement
+(ns witan.cic.tuairisc.care-days
   (:require
    [com.climate.claypoole.lazy :as lazy]
    [net.cgrand.xforms :as x]
    [tablecloth.api :as tc]
    [tech.v3.dataset :as ds]
+   [tech.v3.libs.parquet :as parquet]
    [tech.v3.dataset.reductions :as ds-reduce]
    [tech.v3.datatype.functional :as dfn]
    [tick.core :as tick]
@@ -12,9 +13,12 @@
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
 
-(defn care-days-per-week [{:keys [analysis-start
-                                  analysis-end]}
-                          projection-epsiodes]
+(defn care-days-per-week
+  "This gives sums per age and placement per simulation that can be
+  rolled up and then further analysed."
+  [{:keys [analysis-start
+           analysis-end]}
+   projection-epsiodes]
   (-> projection-epsiodes
       (ds/row-mapcat
        (fn [row]
@@ -36,36 +40,60 @@
                                     :simulation simulation
                                     :placement-type placement-type
                                     :days days)))))
-                   (tick/range (:episode-start row) (:episode-end row))))))
-      (tc/group-by [:simulation :placement-type :age-years :year-week])
-      (tc/aggregate {:days #(dfn/sum (:days %))})))
+                   (tick/range (:episode-start row) (:episode-end row))))))))
 
 (defn mapcat-durations
   "Turn durations of episode-start to episode end into counts of days in
   that duration. Pass in a function that will do this. The default
-  function counts care days per week."
+  function counts care days per week.
+
+  Takes a map like:
+
+  {:projection-episodes (tpe/->ds projection-episodes-csv)
+   :cpu-pool            cpu-pool
+   :analysis-start      (tick/new-date 2018 3 31)
+   :analysis-end        (tick/new-date 2026 4 1)
+   :group-keys [:simulation :placement-type :age-years :year-week]}
+
+  It returns a lazy, unordered sequence of simulations summed by the
+  group-keys which default to:
+   [:simulation :placement-type :age-years :year-week]"
   [{:keys [analysis-start
            analysis-end
            projection-episodes
            mapcatf
+           group-keys
            cpu-pool]
     :or {mapcatf (partial care-days-per-week {:analysis-start analysis-start :analysis-end analysis-end})
+         group-keys [:simulation :placement-type :age-years :year-week]
          cpu-pool (java.util.concurrent.ForkJoinPool/commonPool)}}]
   (as-> projection-episodes $
     (tc/select-columns $ [:simulation :placement-type :birthdate :episode-start :episode-end])
     (tc/select-rows $ (fn [row] (tick/< analysis-start (:episode-end row))))
     (tc/select-rows $ (fn [row] (tick/< (:episode-start row) analysis-end)))
     (tc/group-by $ [:simulation] {:result-type :as-seq})
-    (lazy/upmap cpu-pool mapcatf $)))
+    (lazy/upmap cpu-pool
+                (fn [ds]
+                  (-> ds
+                      mapcatf
+                      (tc/group-by group-keys)
+                      (tc/aggregate {:days #(dfn/sum (:days %))})))
+                $)))
 
-;;(parquet/ds-seq->parquet (str out-dir "placement-year-week-counts-2.parquet") $)
+(defn durations->parquet
+  "This expects a seq of duration data produced from something like mapcat-durations."
+  [duration-data out-file]
+  (parquet/ds-seq->parquet out-file duration-data))
 
-(defn care-days-per-week-by-age [year-weeks]
-  (as-> year-weeks $
-    (tc/group-by $ [:simulation :age-years :year-week])
+(defn parquet->durations [in-file]
+  (parquet/parquet->ds-seq in-file {:key-fn keyword}))
+
+(defn care-days-by-keys [{:keys [domain-key time-key]} simulation-sums]
+  (as-> simulation-sums $
+    (tc/group-by $ [:simulation domain-key time-key])
     (tc/aggregate $ {:days #(dfn/sum (:days %))})
     (ds-reduce/group-by-column-agg
-     [:age-years :year-week]
+     [domain-key time-key]
      {:min     (ds-reduce/prob-quantile :days 0.0)
       :low-95  (ds-reduce/prob-quantile :days 0.05)
       :q1      (ds-reduce/prob-quantile :days 0.25)
@@ -75,24 +103,3 @@
       :max     (ds-reduce/prob-quantile :days 1.0)}
      [$])
     (tc/order-by $ [:age-years :year-week])))
-
-(def age-colors-and-shapes
-  {0  {:color [31.0, 119.0, 180.0, 255.0], :shape \/, :legend-shape \\}
-   1  {:color [255.0, 152.0, 150.0, 255.0], :shape \o, :legend-shape \o}
-   2  {:color [214.0, 39.0, 40.0, 255.0], :shape \A, :legend-shape \V}
-   3  {:color [197.0, 176.0, 213.0, 255.0], :shape \>, :legend-shape \>}
-   4  {:color [148.0, 103.0, 189.0, 255.0], :shape \x, :legend-shape \x}
-   5  {:color [247.0, 182.0, 210.0, 255.0], :shape \v, :legend-shape \^}
-   6  {:color [227.0, 119.0, 194.0, 255.0], :shape \S, :legend-shape \S}
-   7  {:color [196.0, 156.0, 148.0, 255.0], :shape \{, :legend-shape \{}
-   8  {:color [140.0, 86.0, 75.0, 255.0], :shape \s, :legend-shape \s}
-   9  {:color [127.0, 127.0, 127.0, 255.0], :shape \<, :legend-shape \<}
-   10 {:color [219.0, 219.0, 141.0, 255.0], :shape \}, :legend-shape \}}
-   11 {:color [199.0, 199.0, 199.0, 255.0], :shape \-, :legend-shape \-}
-   12 {:color [188.0, 189.0, 34.0, 255.0], :shape \V, :legend-shape \A}
-   13 {:color [158.0, 218.0, 229.0, 255.0], :shape \\, :legend-shape \/}
-   14 {:color [23.0, 190.0, 207.0, 255.0], :shape \^, :legend-shape \v}
-   15 {:color [152.0, 223.0, 138.0, 255.0], :shape \|, :legend-shape \|}
-   16 {:color [255.0, 187.0, 120.0, 255.0], :shape \O, :legend-shape \O}
-   17 {:color [255.0, 127.0, 14.0, 255.0], :shape \/, :legend-shape \\}
-   18 {:color [174.0, 199.0, 232.0, 255.0], :shape \o, :legend-shape \o}})
