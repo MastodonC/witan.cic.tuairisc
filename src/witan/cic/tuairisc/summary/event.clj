09c60f597956332669a800ac053621a3367a08c1
Bruce Durling <bruce@mastodonc.com>
2024-02-09 11:17:50 +0000

    Speed up and fix event summary

--- src/witan/cic/tuairisc/summary/event.clj
+++ src/witan/cic/tuairisc/summary/event.clj
@@ -1,18 +1,24 @@
 (ns witan.cic.tuairisc.summary.event
   (:require
    [tablecloth.api :as tc]
+   [tick.core :as tick]
+   [tech.v3.dataset.reductions :as dsr]
    [witan.cic.tuairisc.summary :as summary]))
 
 (defn summarise
   ([projection-episodes {:keys [event-key analysis-keys analysis-range]}]
    (let [simulation-keys [:simulation]
-         unique-keys (conj [:simulation :id] event-key)]
+         unique-keys (conj [:simulation :id] event-key)
+         end-to-drop (reduce tick/max analysis-range)]
      (as-> projection-episodes $
        (tc/unique-by $ unique-keys)
        (tc/map-rows $ (summary/event-extractor-fn event-key analysis-range))
        (tc/drop-missing $)
-       (tc/group-by $ (into simulation-keys analysis-keys))
-       (tc/aggregate $ {:row-count tc/row-count})
+       (dsr/group-by-column-agg
+        (into simulation-keys analysis-keys)
+        {:row-count (dsr/row-count)}
+        $)
+       (tc/drop-rows $ #(= (% :analysis-date) end-to-drop))
        ((summary/summarise analysis-keys) $))))
   ([{:keys [projection-episodes] :as config}]
    (summarise projection-episodes config)))
